81e8c16925c1e8c6742863d0c15ece7de5c75d9c
nicksonmb <nixyap@gmail.com>
2025-07-06 13:06:17 +0100

    index.html

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,4 +3,4 @@ const currentYear = new Date().getFullYear();
 document.getElementById('currentyear').textContent = new Date().getFullYear();
 
 // Get last modified date
-document.getElementById('lastModified').textContent = document.lastModified;
+document.getElementById('lastupdated').textContent = document.lastModified;
