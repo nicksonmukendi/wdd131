e82de424f10803d5d065506200ecf0360610fff0
nicksonmb <nixyap@gmail.com>
2025-07-06 12:23:20 +0100

    index.html

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,6 @@
 // Get current year for copyright
 const currentYear = new Date().getFullYear();
-document.getElementById('currentyear').textContent = currentYear;
+document.getElementById('currentyear').textContent = new Date().getFullYear();
 
 // Get last modified date
-document.getElementById('lastModified').textContent = `Last Modified: ${document.lastModified}`;
+document.getElementById('lastModified').textContent = document.lastModified;
